b274397d7a7b06adbc3cc5dbffd47da7f2a6536d
Arnav Singh <scholarllong@gmail.com>
2026-09-16 22:28:09 +0530

    adding random forest regressor

---
+++ random-forest/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+X, y = fetch_california_housing(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+rf_regressor = RandomForestRegressor(n_estimators=100, random_state=42)
+
+rf_regressor.fit(X_train, y_train)
+
+y_pred = rf_regressor.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+single_data = X_test[0].reshape(1, -1)
+predicted_value = rf_regressor.predict(single_data)
+print(f"Predicted Value: {predicted_value[0]:.2f}")
+print(f"Actual Value: {y_test[0]:.2f}")
+print(f"Mean Squared Error: {mse:.2f}")
+print(f"R-squared Score: {r2:.2f}")
